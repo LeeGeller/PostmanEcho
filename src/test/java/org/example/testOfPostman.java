31f0a394ca4f57c42b0d5de6b621f2a24a6ec780
Kristina Arkadievna <108188624+LeeGeller@users.noreply.github.com>
2023-06-10 18:02:03 +0300

    first commit

---
+++ src/test/java/org/example/testOfPostman.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class testOfPostman {
+    @Test
+    void testOfPostMetod() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hi, my good friend") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data[3]",equalTo(" "))
+        ;
+    }
+
+}
